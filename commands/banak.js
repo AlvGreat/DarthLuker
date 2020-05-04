286beba72d8c88f78d741434896934a12d25ff04
AlvGreat <60948906+AlvGreat@users.noreply.github.com>
2020-05-03 23:22:21 -0700

    Update banak.js

--- commands/banak.js
+++ commands/banak.js
@@ -1,10 +1,10 @@
 module.exports = {
 	name: 'banak',
-    description: 'ban ak!',
-    aliases: [banakalpit],
+    description: 'banak',
+    aliases: [],
     guildOnly: false,
     cooldown: 1,
 	execute(message, args) {
-        message.channel.send('Can we get a ban in the chat for <@619723116435734530>?'); 
+		message.channel.send('Can we get a ban in the chat for <@619723116435734530>?');
 	},
 };
