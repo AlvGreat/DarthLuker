7d5c94da64700e2d4e151f9d943c4319153a0b29
AlvGreat <60948906+AlvGreat@users.noreply.github.com>
2020-05-14 17:10:28 -0700

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -5,21 +5,19 @@ module.exports = {
     guildOnly: true,
     cooldown: 1,
 	execute(message, args) {
-		if (member.roles.cache.some(role => role.name === '-m')) {
-			const amount = parseInt(args[0]);
-
-			if (isNaN(amount)) {
-			    return message.reply('that doesn\'t seem to be a valid number.');
-			} 
-			else if (amount < 1 || amount > 99) {
-			    return message.reply('Can only delete a number between 1 and 99.');
-			} 
-			else {
-			    message.channel.bulkDelete(amount + 1, true).catch(err => {
-				console.error(err);
-				message.channel.send('Error: cannot delete messages.');
-			    });
-			}
-		}
+		message.reply("This command is currently disabled");
+		/*
+		if (isNaN(amount)) {
+		    return message.reply('that doesn\'t seem to be a valid number.');
+		} 
+		else if (amount < 1 || amount > 99) {
+		    return message.reply('Can only delete a number between 1 and 99.');
+		} 
+		else {
+		    message.channel.bulkDelete(amount + 1, true).catch(err => {
+			console.error(err);
+			message.channel.send('Error: cannot delete messages.');
+		    });
+		}*/
 	},
 };
